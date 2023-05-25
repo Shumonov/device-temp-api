4a1a7c09456dd3e72fdc1678e779c02b1e19ff63
salexander19 <salexandercode@gmail.com>
2023-05-25 13:29:30 -0700

    sets DATABASE_URL to the heroku one

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Psql123456!@localhost/device_errors'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
