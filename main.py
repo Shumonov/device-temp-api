0533988cfc9fac81bbc73c3eaec9c347ac8907a3
salexander19 <salexandercode@gmail.com>
2023-05-25 13:38:45 -0700

    changes the config var name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
